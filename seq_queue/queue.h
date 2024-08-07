8d6bda8367f88d40e7fcef0380a37aca43015adc
Gabriel Ayres <gabn2012@gmail.com>
2024-08-07 00:31:03 -0300

    readme linked list done

--- seq_queue/queue.h
+++ seq_queue/queue.h
@@ -4,19 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXTAM 100000
+#define MAXTAM 10000
 
 typedef struct Queue Queue;
 
 struct Queue{
-
     int *data;
     int begin;
     int end;
-
+    int size;
 };
 
+Queue* createQueue();
 
+int empty(Queue* q);
 
+int full(Queue* q);
+
+void enqueue(Queue* q, int data);
+
+void dequeue(Queue* q);
+
+int getSize(Queue* q);
 
 #endif
